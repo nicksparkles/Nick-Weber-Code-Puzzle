aff0f19fce9b28c779de88ed699bc57e4d338590
Nick Weber <nweber6@gmail.com>
2025-10-01 21:53:57 -0500

    Calling code from cmd line
    Using a default input when none passed in

--- RedRoverCodePuzzle/Program.cs
+++ RedRoverCodePuzzle/Program.cs
@@ -6,7 +6,18 @@ namespace RedRoverCodePuzzle
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Entry Point - with default arg value
+            var stringToFormat = args.Length > 0 ?
+                args[0]
+                :
+                "(id, name, email, type(id, name, customFields(c1, c2, c3)), externalId)";
+
+            Console.WriteLine($"{Environment.NewLine}***Standard Format***");
+            Console.WriteLine(StringHelper.FormatString(stringToFormat));
+
+            Console.WriteLine($"{Environment.NewLine}***Alphabetical Format***");         
+            Console.WriteLine(StringHelper.FormatString(stringToFormat, FormatOptions.Alphabetical));
+
         }
     }
 }
